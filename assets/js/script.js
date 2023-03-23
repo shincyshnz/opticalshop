c0486e9c7c89192e74df068ebde3fbb13ffc0913
Shincy <shincyshnz@gmail.com>
2023-03-23 15:01:48 +0530

    finished

--- assets/js/script.js
+++ assets/js/script.js
@@ -8,14 +8,36 @@ const header = document.querySelector('.header');
 const nav = document.querySelector('.nav');
 const navHeight = nav.getBoundingClientRect().height;
 const logo = document.querySelector('.logo-link');
+const mediaBtnChildren = document.querySelectorAll('.media-button-span');
+const navLinks = document.querySelector('.nav-links');
+const allSections = document.querySelectorAll('.section');
+
+// ///////////////////////////////////////
+// Functions
+
+const mediaBtnActive = function () {
+    nav.classList.add('sticky');
+    logo.classList.add('logoColor');
+    mediaBtnChildren.forEach(child => child.style.backgroundColor = 'white');
+};
+
+const mediaBtnDeactive = function () {
+    nav.classList.remove('sticky');
+    logo.classList.remove('logoColor');
+    mediaBtnChildren.forEach(child => child.style.backgroundColor = 'var(--theme-color-logo)');
+};
+
+const removeNavLinkClick = function () {
+    document.querySelectorAll('.nav-link').forEach(navLink => navLink.classList.remove('clicked'));
+};
 
 ///////////////////////////////////////
-// burger menu
-mediaButton.onclick = function () {
+// burger menu click event
+mediaButton.addEventListener('click', function () {
     mainListDiv.classList.toggle("show_list");
     mediaButton.classList.toggle("active");
-
-};
+    mediaBtnActive();
+});
 
 ///////////////////////////////////////
 // Sticky navigation: Intersection Observer API
@@ -23,12 +45,13 @@ mediaButton.onclick = function () {
 const stickyNav = function (entries) {
     const [entry] = entries;
     if (!entry.isIntersecting) {
-        nav.classList.add('sticky');
-        logo.classList.add('logoColor');
+        mediaBtnActive();
     }
     else {
-        nav.classList.remove('sticky');
-        logo.classList.remove('logoColor');
+        mainListDiv.classList.remove("show_list");
+        mediaButton.classList.remove("active");
+        mediaBtnDeactive();
+        removeNavLinkClick();
     }
 }
 
@@ -39,3 +62,37 @@ const headerObserver = new IntersectionObserver(stickyNav, {
 });
 
 headerObserver.observe(header);
+
+///////////////////////////////////////
+// Smooth Scrolling
+
+navLinks.addEventListener('click', function (e) {
+    e.preventDefault();
+    removeNavLinkClick();
+
+    e.target.classList.add('clicked');
+    if (e.target.classList.contains('nav-link')) {
+        const id = e.target.getAttribute('href');
+        document.querySelector(id).scrollIntoView({ behavior: 'smooth' });
+    }
+});
+
+///////////////////////////////////////
+// Reveal sections
+const revealSection = function (entries, observer) {
+    const [entry] = entries;
+
+    if (!entry.isIntersecting) return;
+    entry.target.classList.remove('section-hidden');
+    observer.unobserve(entry.target);
+};
+
+const sectionObserver = new IntersectionObserver(revealSection, {
+    root: null,
+    threshold: 0.15,
+});
+
+allSections.forEach(section => {
+    sectionObserver.observe(section);
+    section.classList.add('section-hidden');
+});
